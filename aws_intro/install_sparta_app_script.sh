c2c88c3c4e29544e4b54e2a2ec0324dbd0fd6def
Spencer Ley <spencerley234@gmail.com>
2024-04-10 14:31:09 +0100

    working on terraform

--- aws_intro/install_sparta_app_script.sh
+++ aws_intro/install_sparta_app_script.sh
@@ -13,7 +13,7 @@ sudo apt update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 
 # DB_HOST - export DB_HOST=mongodb://<yourIpHere>:27017/posts
-# export DB_HOST=mongodb://10.0.3.4:27017/posts
+# export DB_HOST=mongodb://172.31.59.112:27017/posts
 
 # nginx -
 sudo DEBIAN_FRONTEND=noninteractive apt install nginx -y
